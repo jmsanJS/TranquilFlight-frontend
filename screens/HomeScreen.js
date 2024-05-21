7bdf847b0fdd95c136fbc9eab2a9a058488b88a6
Juan Sanchez <juanboca90@gmail.com>
2024-05-21 15:38:44 +0200

    HomeScreen flight number and input focus in signinScreen

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,21 +1,145 @@
-import { View, Text, StyleSheet, SafeAreaView, Platform, StatusBar } from 'react-native';
+import {
+  View,
+  Text,
+  StyleSheet,
+  SafeAreaView,
+  Platform,
+  StatusBar,
+  Image,
+  TextInput,
+  TouchableOpacity,
+  ImageBackground,
+} from "react-native";
+import { useState } from "react";
+import { useSelector } from "react-redux";
 
-import {colors} from '../assets/colors'
+import { colors } from "../assets/colors";
 
 export default function HomeScreen() {
-    return (
-        <SafeAreaView style={styles.container}>
-            <Text>HomeScreen</Text>
-        </SafeAreaView>
-    );
+  const [isFocused, setIsFocused] = useState({ email: false, password: false });
+
+  const user = useSelector((state) => state.user.value);
+  // console.log(user);
+  const handleFocus = (key) => {
+    setIsFocused((prevState) => ({ ...prevState, [key]: true }));
+  };
+
+  const handleBlur = (key) => {
+    setIsFocused((prevState) => ({ ...prevState, [key]: false }));
+  };
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <ImageBackground
+        source={require("../assets/home-image.jpg")}
+        style={styles.homeImage}
+        alt="Home image"
+      >
+        {user.firstname ? (
+          <Text style={styles.welcomeUserIfConnected}>
+            Bonjour {user.firstname}
+          </Text>
+        ) : null}
+      </ImageBackground>
+      <View style={styles.searchContainer}>
+        <View style={styles.dropShadow}>
+          <View
+            style={[styles.fieldSet, isFocused.email && styles.focusedInput]}
+          >
+            <Text style={styles.legend}>N° de vol</Text>
+            <TextInput
+              cursorColor={colors.light1}
+              allowFontScaling={true}
+              keyboardType="email-address"
+              autoCapitalize="none"
+              style={styles.inputs}
+              onChangeText={(value) => setEmail(value)}
+              onFocus={() => handleFocus("email")}
+              onBlur={() => handleBlur("email")}
+            ></TextInput>
+          </View>
+        </View>
+        <TouchableOpacity style={styles.searchBtn}>
+          <Text style={styles.searchBtnText}>Rechercher</Text>
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex:1,
-        paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
-        justifyContent:'center',
-        alignItems:'center',
-        backgroundColor:colors.lightGrey,
-    },
+  container: {
+    flex: 1,
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: colors.lightGrey,
+  },
+  homeImage: {
+    resizeMode: "cover",
+    width: "100%",
+    flex: 5,
+  },
+  welcomeUserIfConnected: {
+    margin: 20,
+    color: "white",
+    fontSize: 25,
+  },
+  searchContainer: {
+    flex: 3,
+    width: "100%",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  dropShadow: {
+    width: "80%",
+    marginBottom: 20,
+    borderRadius: 5,
+  },
+  fieldSet: {
+    width: "100%",
+    paddingHorizontal: 5,
+    paddingTop: 5,
+    paddingBottom: 5,
+    borderRadius: 5,
+    borderWidth: 2,
+    alignItems: "center",
+    borderColor: "#000",
+    borderColor: colors.dark1,
+    backgroundColor: colors.lightGrey,
+  },
+  legend: {
+    position: "absolute",
+    top: -10,
+    left: 10,
+    fontWeight: "600",
+    paddingHorizontal: 3,
+    fontSize: 11,
+    backgroundColor: colors.lightGrey,
+    color: colors.dark1,
+  },
+  inputs: {
+    width: "100%",
+    justifyContent: "center",
+    height: 40,
+    color: colors.light1,
+  },
+  focusedInput: {
+    borderWidth: 3,
+    borderRadius: 3,
+  },
+  searchBtn: {
+    width: "80%",
+    backgroundColor: colors.hot1,
+    borderRadius: 5,
+    justifyContent: "center",
+    alignItems: "center",
+    height: 50,
+  },
+  searchBtnText: {
+    alignSelf: "center",
+    color: "white",
+    fontWeight: "600",
+    fontSize: 15,
+  },
 });
--- screens/SigninScreen.js
+++ screens/SigninScreen.js
@@ -1,216 +1,210 @@
-import { View, Text, StyleSheet, Platform, StatusBar, TouchableOpacity, TextInput, KeyboardAvoidingView } from 'react-native';
-import { useState } from 'react'
+import {
+  View,
+  Text,
+  StyleSheet,
+  Platform,
+  StatusBar,
+  TouchableOpacity,
+  TextInput,
+  KeyboardAvoidingView,
+} from "react-native";
+import { useState } from "react";
 
-import { colors } from '../assets/colors'
+import { colors } from "../assets/colors";
 
-import * as Crypto from 'expo-crypto';
+import * as Crypto from "expo-crypto";
 
-import { useDispatch, useSelector } from 'react-redux';
-import { login } from '../reducers/user';
+import { useDispatch, useSelector } from "react-redux";
+import { login } from "../reducers/user";
 
-export default function SigninScreen({navigation}) {
+export default function SigninScreen({ navigation }) {
+  const user = useSelector((state) => state.user.value);
 
-    const user = useSelector((state) => state.user.value);
+  if (user.email != null) {
+    navigation.navigate("Mon Compte");
+  }
 
-    if (user.email != null){
-        navigation.navigate('Mon Compte')
-    }
+  const dispatch = useDispatch();
 
-    const dispatch = useDispatch();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+  const [isFocused, setIsFocused] = useState({ email: false, password: false });
 
-    const [email, setEmail]=useState('')
-    const [password, setPassword]=useState('')
-    const [errorMessage, setErrorMessage]=useState('')
+  const handleFocus = (key) => {
+    setIsFocused((prevState) => ({ ...prevState, [key]: true }));
+  };
 
-    const handleSubmit = async () => {
-        console.log('front')
-        const hashedPassword = await Crypto.digestStringAsync(
-            Crypto.CryptoDigestAlgorithm.SHA256, password
-        )
-        console.log(hashedPassword)
-        fetch('http://localhost:3000/user/signin', {
-			method: 'POST',
-			headers: { 'Content-Type': 'application/json' },
-			body: JSON.stringify({ email: email, password: hashedPassword }),
-		}).then(response => response.json())
-        .then(data => {
-            if (data.result) {
-                setEmail('');
-                setPassword('');
-                dispatch(login(data.userData));
-                navigation.navigate('Mon Compte')
-            }else if (data.result===false){
-                setErrorMessage(data.error)
-            }
-        });
-    }
+  const handleBlur = (key) => {
+    setIsFocused((prevState) => ({ ...prevState, [key]: false }));
+  };
 
-    return (
-        <KeyboardAvoidingView
-            behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
-            style={styles.container}>
-
-            <View style={styles.ApisConnectContainer}>
-                <TouchableOpacity style={styles.ApisConnectBtn}>
-                    <Text style={styles.ApisConnectTxt}>Connexion Via Google</Text>
-                </TouchableOpacity>
-                <TouchableOpacity style={styles.ApisConnectBtn}>
-                    <Text style={styles.ApisConnectTxt}>Connexion via Facebook</Text>
-                </TouchableOpacity>
-                <TouchableOpacity style={styles.ApisConnectBtn}>
-                    <Text style={styles.ApisConnectTxt}>Connexion via Apple</Text>
-                </TouchableOpacity>
-            </View>
-
-            <View style={styles.separatorContainer}>
-                <View style={styles.separator}></View>
-                <Text style={styles.separatorTxt}>OU</Text>
-                <View style={styles.separator}></View>
-            </View>
-
-            <View style={styles.emailConnectContainer}>
-                <View style={styles.errorMessageContainer}>
-                    <Text style={styles.errorMessage}>{errorMessage}</Text>
-                </View>
-                <View style={styles.dropShadow}>
-                    <View style={styles.fieldSet}>
-                        <Text style={styles.legend}>Email</Text>
-                        <TextInput cursorColor={colors.light1} allowFontScaling={true} keyboardType="email-address" style={styles.inputs} onChangeText={(value)=>setEmail(value)}></TextInput>
-                    </View>
-                </View>
-                <View style={styles.dropShadow}>
-                    <View style={styles.fieldSet}>
-                        <Text style={styles.legend}>Mot de passe</Text>
-                        <TextInput cursorColor={colors.light1} allowFontScaling={true} secureTextEntry={true} textContentType={'password'} style={styles.inputs} onChangeText={(value)=>setPassword(value)}></TextInput>
-                    </View>
-                </View>
-                <TouchableOpacity style={styles.loginBtn} onPress={()=>handleSubmit()}>
-                    <Text style={styles.loginBtnTxt}>Connexion</Text>
-                </TouchableOpacity>
-                <TouchableOpacity style={styles.createAccount} onPress={()=>navigation.navigate('Créer un compte')}>
-                    <Text style={styles.createAccountTxt}>Créer un compte</Text>
-                </TouchableOpacity>
-            </View>
-
-        </KeyboardAvoidingView>
+  const handleSubmit = async () => {
+    console.log("front");
+    const hashedPassword = await Crypto.digestStringAsync(
+      Crypto.CryptoDigestAlgorithm.SHA256,
+      password
     );
+    console.log(hashedPassword);
+    fetch("http://localhost:3000/user/signin", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ email: email, password: hashedPassword }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.result) {
+          setEmail("");
+          setPassword("");
+          dispatch(login(data.userData));
+          navigation.navigate("Mon Compte");
+        } else if (data.result === false) {
+          setErrorMessage(data.error);
+        }
+      });
+  };
+
+  return (
+    <KeyboardAvoidingView
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+      style={styles.container}
+    >
+      <View style={styles.emailConnectContainer}>
+        <View style={styles.errorMessageContainer}>
+          <Text style={styles.errorMessage}>{errorMessage}</Text>
+        </View>
+        <View style={styles.dropShadow}>
+          <View
+            style={[styles.fieldSet, isFocused.email && styles.focusedInput]}
+          >
+            <Text style={styles.legend}>Email</Text>
+            <TextInput
+              cursorColor={colors.light1}
+              allowFontScaling={true}
+              keyboardType="email-address"
+              autoCapitalize="none"
+              style={styles.inputs}
+              onChangeText={(value) => setEmail(value)}
+              onFocus={() => handleFocus("email")}
+              onBlur={() => handleBlur("email")}
+            ></TextInput>
+          </View>
+        </View>
+        <View style={styles.dropShadow}>
+          <View
+            style={[styles.fieldSet, isFocused.password && styles.focusedInput]}
+          >
+            <Text style={styles.legend}>Mot de passe</Text>
+            <TextInput
+              cursorColor={colors.light1}
+              allowFontScaling={true}
+              secureTextEntry={true}
+              textContentType={"password"}
+              autoCapitalize="none"
+              style={styles.inputs}
+              onChangeText={(value) => setPassword(value)}
+              onFocus={() => handleFocus("password")}
+              onBlur={() => handleBlur("password")}
+            ></TextInput>
+          </View>
+        </View>
+        <TouchableOpacity
+          style={styles.loginBtn}
+          onPress={() => handleSubmit()}
+        >
+          <Text style={styles.loginBtnTxt}>Connexion</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.createAccount}
+          onPress={() => navigation.navigate("Créer un compte")}
+        >
+          <Text style={styles.createAccountTxt}>Créer un compte</Text>
+        </TouchableOpacity>
+      </View>
+    </KeyboardAvoidingView>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex:1,
-        paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
-        justifyContent:'center',
-        alignItems:'center',
-        backgroundColor:colors.lightGrey,
-        marginTop:-30,
-    },
-    ApisConnectContainer:{
-        flex:1,
-        width:'100%',
-        justifyContent:'center',
-        alignItems:'center',
-    },
-    ApisConnectBtn:{
-        width:'80%',
-        backgroundColor:colors.dark1,
-        borderRadius:5,
-        justifyContent:'center',
-        alignItems:'center',
-        height:50,
-        marginBottom:20,
-    },
-    ApisConnectTxt:{
-        alignSelf:'center',
-        color:'white',
-        fontWeight:'600',
-        fontSize:15,
-    },
-    separatorContainer:{
-        width:'80%',
-        display:'flex',
-        flexDirection:'row',
-        justifyContent:'space-between',
-        alignItems:'center',
-        marginTop:-20,
-    },
-    separator:{
-        borderColor:colors.dark1,
-        borderBottomWidth:1,
-        width:'40%',
-    },
-    separatorTxt:{
-        color:colors.dark1,
-        fontWeight:'600'
-    },
-    emailConnectContainer:{
-        flex:1,
-        width:'100%',
-        justifyContent:'center',
-        alignItems:'center',
-    },
-    errorMessageContainer:{
-        width:'80%',
-        marginBottom:2
-    },
-    errorMessage:{
-        color:'red',
-        alignSelf:'flex-end',
-        fontSize:11
-    },
-    dropShadow:{
-        width:'80%',
-        marginBottom:20,
-        borderRadius: 5,
-    },
-    fieldSet:{
-        width:'100%',
-        paddingHorizontal: 5,
-        paddingTop: 5,
-        paddingBottom: 5,
-        borderRadius: 5,
-        borderWidth: 2,
-        alignItems: 'center',
-        borderColor: '#000',
-        borderColor: colors.dark1,
-        backgroundColor : colors.lightGrey,
-        
-    },
-    legend:{
-        position: 'absolute',
-        top: -10,
-        left: 10,
-        fontWeight: '600',
-        paddingHorizontal:3,
-        fontSize:11,
-        backgroundColor: colors.lightGrey,
-        color:colors.dark1,
-    },
-    inputs:{
-        width:'100%',
-        justifyContent:'center',
-        height:40,
-        color:colors.light1,
-    },
-    loginBtn:{
-        width:'80%',
-        backgroundColor:colors.dark1,
-        borderRadius:5,
-        justifyContent:'center',
-        alignItems:'center',
-        height:50,
-    },
-    loginBtnTxt:{
-        alignSelf:'center',
-        color:'white',
-        fontWeight:'600',
-        fontSize:15,
-    },
-    createAccount:{
-        marginTop:15
-    },
-    createAccountTxt:{
-        textDecorationLine:'underline',
-        color:colors.dark1
-    }
+  container: {
+    flex: 1,
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: colors.lightGrey,
+    marginTop: -30,
+  },
+  emailConnectContainer: {
+    flex: 1,
+    width: "100%",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  errorMessageContainer: {
+    width: "80%",
+    marginBottom: 2,
+  },
+  errorMessage: {
+    color: "red",
+    alignSelf: "flex-end",
+    fontSize: 11,
+  },
+  dropShadow: {
+    width: "80%",
+    marginBottom: 20,
+    borderRadius: 5,
+  },
+  fieldSet: {
+    width: "100%",
+    paddingHorizontal: 5,
+    paddingTop: 5,
+    paddingBottom: 5,
+    borderRadius: 5,
+    borderWidth: 2,
+    alignItems: "center",
+    borderColor: "#000",
+    borderColor: colors.dark1,
+    backgroundColor: colors.lightGrey,
+  },
+  legend: {
+    position: "absolute",
+    top: -10,
+    left: 10,
+    fontWeight: "600",
+    paddingHorizontal: 3,
+    fontSize: 11,
+    backgroundColor: colors.lightGrey,
+    color: colors.dark1,
+  },
+  inputs: {
+    width: "100%",
+    justifyContent: "center",
+    height: 40,
+    color: colors.light1,
+  },
+  focusedInput: {
+    borderWidth: 3,
+    borderRadius: 3,
+  },
+  loginBtn: {
+    width: "80%",
+    backgroundColor: colors.dark1,
+    borderRadius: 5,
+    justifyContent: "center",
+    alignItems: "center",
+    height: 50,
+  },
+  loginBtnTxt: {
+    alignSelf: "center",
+    color: "white",
+    fontWeight: "600",
+    fontSize: 15,
+  },
+  createAccount: {
+    marginTop: 15,
+  },
+  createAccountTxt: {
+    textDecorationLine: "underline",
+    color: colors.dark1,
+  },
 });
