0bd60ce927e16431a3e7f204e15abe22a108f19a
Mehdi <mehdi.karim43@gmail.com>
2024-05-22 15:34:28 +0200

    création de component flightCard (fichiers) non peuplé

---
+++ components/flightCard.js
@@ -0,0 +1,24 @@
+import {
+    View,
+    Text,
+    StyleSheet,
+    SafeAreaView,
+    Platform,
+    StatusBar,
+    TextInput,
+    TouchableOpacity,
+    ImageBackground,
+    KeyboardAvoidingView,
+} from "react-native";
+
+function FlightCard(props) {
+
+
+	return (
+		<View>
+			
+		</View>
+	);
+}
+
+export default FlightCard;
