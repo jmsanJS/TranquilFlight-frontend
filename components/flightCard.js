b1b776db52194c0cac78937ff5c772fdb8910b6d
Juan Sanchez <juanboca90@gmail.com>
2024-05-22 17:52:25 +0200

    Component flightCard et implementation dans le SearchResultScreen

--- components/flightCard.js
+++ components/flightCard.js
@@ -1,24 +1,169 @@
-import {
-    View,
-    Text,
-    StyleSheet,
-    SafeAreaView,
-    Platform,
-    StatusBar,
-    TextInput,
-    TouchableOpacity,
-    ImageBackground,
-    KeyboardAvoidingView,
-} from "react-native";
+import { View, Text, StyleSheet } from "react-native";
+import { colors } from "../assets/colors";
+import FontAwesome from "react-native-vector-icons/FontAwesome";
+import FontAwesome5 from '@expo/vector-icons/FontAwesome5';
 
 function FlightCard(props) {
-
-
-	return (
-		<View>
-			
-		</View>
-	);
+  return (
+    <View style={styles.favoriteTripContainer}>
+      <View style={styles.favoriteTripHeader}>
+        <View style={styles.flightNumberContainer}>
+          <Text style={styles.flightNumberText}>N0301</Text>
+          <Text style={styles.companyText}>Air France</Text>
+        </View>
+        <Text style={styles.date}>19-06-2024</Text>
+        <View style={styles.icons}>
+          <FontAwesome name="bell" size={25} color={colors.dark1} />
+          <FontAwesome
+            name="heart"
+            size={25}
+            color={colors.dark1}
+            style={styles.heartIcon}
+          />
+        </View>
+      </View>
+      <View style={styles.favoriteTripDescriptionContainer}>
+        <View style={styles.departureInfo}>
+          <Text style={styles.departureTime}>14:56</Text>
+          <Text style={styles.departureCity}>Paris</Text>
+          <Text style={styles.departureAirportCode}>ORY</Text>
+        </View>
+        <View style={styles.routeLineContainer}>
+          <Text style={styles.tripTime}>1h08</Text>
+          <View style={styles.routeLineAndTripTime}>
+            <View style={styles.routeLine}></View>
+            <FontAwesome5
+              name="plane"
+              size={13}
+              color={colors.dark1}
+              style={styles.planeIcon}
+            />
+          </View>
+        </View>
+        <View style={styles.arrivalInfo}>
+          <Text style={styles.arrivalTime}>16:04</Text>
+          <Text style={styles.arrivalCity}>Lyon</Text>
+          <Text style={styles.arrivalAirportCode}>LYS</Text>
+        </View>
+      </View>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  favoriteTripContainer: {
+    backgroundColor: "white",
+    width: "80%",
+    marginBottom: 10,
+    padding: 10,
+    borderRadius: 5,
+  },
+  favoriteTripHeader: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginBottom: 10,
+  },
+  flightNumberContainer: {
+    width: "33.33%",
+  },
+  flightNumberText: {
+    color: colors.light1,
+    fontWeight: "600",
+    fontSize: 13,
+  },
+  companyText: {
+    color: colors.dark1,
+    fontSize: 11,
+    fontWeight: "600",
+    marginTop: -5,
+  },
+  date: {
+    width: "33.33%",
+    color: colors.dark1,
+    fontSize: 13,
+    fontWeight: "600",
+    width: "33.33%",
+    textAlign: "center",
+  },
+  icons: {
+    flexDirection: "row",
+    width: "33.33%",
+    justifyContent: "flex-end",
+  },
+  heartIcon: {
+    marginLeft: 10,
+  },
+  favoriteTripDescriptionContainer: {
+    flexDirection: "row",
+    justifyContent: "center",
+    paddingHorizontal: 10,
+  },
+  departureInfo: {
+    textAlign: "right",
+    width: "25%",
+  },
+  departureTime: {
+    textAlign: "right",
+    fontSize: 20,
+    fontWeight: "600",
+    color: colors.dark1,
+  },
+  departureCity: {
+    textAlign: "right",
+    fontSize: 14,
+    fontWeight: "700",
+    marginTop: -5,
+    color: colors.dark1,
+  },
+  departureAirportCode: {
+    textAlign: "right",
+    fontSize: 12,
+    fontWeight: "600",
+    marginTop: -3,
+    color: colors.dark1,
+  },
+  routeLineContainer: {
+    width: "55%",
+    paddingHorizontal: 10,
+  },
+  routeLineAndTripTime: {
+    flexDirection: "row",
+    width: "90%",
+    alignItems: "center",
+  },
+  tripTime: {
+    textAlign: "center",
+    color: colors.light1,
+    fontSize: 12,
+    marginTop: 5,
+    marginBottom: -5,
+  },
+  routeLine: {
+    borderColor: colors.dark1,
+    borderBottomWidth: 1,
+    width: "100%",
+    borderColor: colors.hot1,
+  },
+  arrivalInfo: {
+    width: "25%",
+  },
+  arrivalTime: {
+    fontSize: 20,
+    fontWeight: "600",
+    color: colors.dark1,
+  },
+  arrivalCity: {
+    fontSize: 14,
+    fontWeight: "700",
+    marginTop: -5,
+    color: colors.dark1,
+  },
+  arrivalAirportCode: {
+    fontSize: 12,
+    fontWeight: "600",
+    marginTop: -3,
+    color: colors.dark1,
+  },
+});
 
 export default FlightCard;
--- screens/SearchResultScreen.js
+++ screens/SearchResultScreen.js
@@ -19,9 +19,10 @@ import { addFlight } from "../reducers/flightsResult";
 
 import { colors } from "../assets/colors";
 import FontAwesome from "react-native-vector-icons/FontAwesome";
-import FontAwesome5 from "@expo/vector-icons/FontAwesome5";
 
-export default function HomeScreen({ navigation }) {
+import FlightCard from "../components/flightCard";
+
+export default function SearchResultScreen({ navigation }) {
   const [isFocused, setIsFocused] = useState({
     flightNumber: false,
     flightDate: false,
@@ -148,48 +149,7 @@ export default function HomeScreen({ navigation }) {
       </View>
 
       <View style={styles.favoritesTripsContainer}>
-        <View style={styles.favoriteTrip}>
-          <View style={styles.favoriteTripHeader}>
-            <View style={styles.flightNumberContainer}>
-              <Text style={styles.flightNumberText}>N0301</Text>
-              <Text style={styles.companyText}>Air France</Text>
-            </View>
-            <Text style={styles.date}>19-06-2024</Text>
-            <View style={styles.icons}>
-              <FontAwesome name="bell" size={25} color={colors.dark1} />
-              <FontAwesome
-                name="heart"
-                size={25}
-                color={colors.dark1}
-                style={styles.heartIcon}
-              />
-            </View>
-          </View>
-          <View style={styles.favoriteTripDescriptionContainer}>
-            <View style={styles.departureInfo}>
-              <Text style={styles.departureTime}>14:56</Text>
-              <Text style={styles.departureCity}>Paris</Text>
-              <Text style={styles.departureAirportCode}>ORY</Text>
-            </View>
-            <View style={styles.routeLineContainer}>
-              <Text style={styles.tripTime}>1h08</Text>
-              <View style={styles.routeLineAndTripTime}>
-                <View style={styles.routeLine}></View>
-                <FontAwesome5
-                  name="plane"
-                  size={13}
-                  color={colors.dark1}
-                  style={styles.planeIcon}
-                />
-              </View>
-            </View>
-            <View style={styles.arrivalInfo}>
-              <Text style={styles.arrivalTime}>16:04</Text>
-              <Text style={styles.arrivalCity}>Lyon</Text>
-              <Text style={styles.arrivalAirportCode}>LYS</Text>
-            </View>
-          </View>
-        </View>
+        <FlightCard />
       </View>
     </SafeAreaView>
   );
@@ -298,127 +258,11 @@ const styles = StyleSheet.create({
     fontSize: 7,
   },
   planeIcon: {
-    // paddingTop: 12.5
     justifyContent: "flex-end",
   },
   favoritesTripsContainer: {
     width: "100%",
     alignItems: "center",
     paddingTop: 10,
-  },
-
-  // Flight Card
-  favoriteTrip: {
-    backgroundColor: "white",
-    width: "80%",
-    marginBottom: 10,
-    padding: 10,
-    borderRadius: 5,
-  },
-  favoriteTripHeader: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-    marginBottom: 10,
-  },
-  flightNumberContainer: {
-    width: "33.33%",
-  },
-  flightNumberText: {
-    color: colors.light1,
-    fontWeight: "600",
-    fontSize: 13,
-  },
-  companyText: {
-    color: colors.dark1,
-    fontSize: 11,
-    fontWeight: "600",
-    marginTop: -5,
-  },
-  date: {
-    width: "33.33%",
-    color: colors.dark1,
-    fontSize: 13,
-    fontWeight: "600",
-    width: "33.33%",
-    textAlign: "center",
-  },
-  icons: {
-    flexDirection: "row",
-    width: "33.33%",
-    justifyContent: "flex-end",
-  },
-  heartIcon: {
-    marginLeft: 10,
-  },
-  favoriteTripDescriptionContainer: {
-    flexDirection: "row",
-    justifyContent: "center",
-    paddingHorizontal: 10,
-  },
-  departureInfo: {
-    textAlign: "right",
-    width: "25%",
-  },
-  departureTime: {
-    textAlign: "right",
-    fontSize: 20,
-    fontWeight: "600",
-    color: colors.dark1,
-  },
-  departureCity: {
-    textAlign: "right",
-    fontSize: 14,
-    fontWeight: "700",
-    marginTop: -5,
-    color: colors.dark1,
-  },
-  departureAirportCode: {
-    textAlign: "right",
-    fontSize: 12,
-    fontWeight: "600",
-    marginTop: -3,
-    color: colors.dark1,
-  },
-  routeLineContainer: {
-    width: "55%",
-    paddingHorizontal: 10,
-  },
-  routeLineAndTripTime: {
-    flexDirection: "row",
-    width: "90%",
-    alignItems: "center",
-  },
-  tripTime: {
-    textAlign: "center",
-    color: colors.light1,
-    fontSize: 12,
-    marginTop: 5,
-    marginBottom: -5
-  },
-  routeLine: {
-    borderColor: colors.dark1,
-    borderBottomWidth: 1,
-    width: "100%",
-    borderColor: colors.hot1,
-  },
-  arrivalInfo: {
-    width: "25%",
-  },
-  arrivalTime: {
-    fontSize: 20,
-    fontWeight: "600",
-    color: colors.dark1,
-  },
-  arrivalCity: {
-    fontSize: 14,
-    fontWeight: "700",
-    marginTop: -5,
-    color: colors.dark1,
-  },
-  arrivalAirportCode: {
-    fontSize: 12,
-    fontWeight: "600",
-    marginTop: -3,
-    color: colors.dark1,
   },
 });
